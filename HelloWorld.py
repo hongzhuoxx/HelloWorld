200860d8a17824f67b64f8bab366f761f3744771
hongzhuoxx <417697172@qq.com>
2016-10-02 14:53:01 +0800

    HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -23,7 +23,20 @@ for i in range(100):
     sys.stdout.write("Data:" + str(i) + "% \r")
     sys.stdout.flush()
     time.sleep(0.1)
-
+else:
+    print("Data-OK")
 
 
 print ("Data:100%")
+
+dic_l={"Test_A":"00%","Test_B":"20%","Test_C":"40%","Test_D":"60%","Test_E":"80%","Test_F":"100%"}
+dic_l= sorted(dic_l.items(),key=lambda d:d[0])
+
+for i in dic_l:
+    sys.stdout.write("dic_data Key:" + i[0] + " Value:" + i[1] + "\r")
+    sys.stdout.flush()
+    time.sleep(1)
+
+print ("\n")
+print ("OK!")
+
